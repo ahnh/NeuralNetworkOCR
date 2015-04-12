835baa1a9da9f83d7336f79c93449e6997ef37da
Heesung Ahn <ahnh@mail.uoguelph.ca>
2015-04-12 17:20:57 -0400

    update to read all trained files

--- nnocr.py
+++ nnocr.py
@@ -17,27 +17,7 @@ def showImage(img):
     imshow(img, cmap=cm.gray)
     show()
 
-if __name__ == "__main__":
-    file = "3notanhx1.xml"
-    print file
-    if  os.path.isfile(file):
-        print "reading network"
-        net = NetworkReader.readFrom(file)
-    else:
-        train_ds = SupervisedDataSet(28 * 28, 1)
-        train_images, train_labels = mnist.load_mnist(dataset='training')
-        for image, label in zip(train_images, train_labels):
-            train_ds.addSample(ravel(image), label)
-
-        #net = buildNetwork(train_ds.indim, 98, 98, 49, train_ds.outdim, bias=True, hiddenclass=TanhLayer)
-        net = buildNetwork(train_ds.indim, 98, train_ds.outdim, bias=True)
-        trainer = BackpropTrainer(net, train_ds)
-
-        print "start training"
-        for i in range(1):
-            trainer.trainEpochs(1)
-            print "epoch: %4d" % trainer.totalepochs
-
+def testNetwork(net):
     for i in range(0, 10):
         num = i
         test_ds = SupervisedDataSet(28 * 28, 1)
@@ -51,4 +31,38 @@ if __name__ == "__main__":
         count = sum(res == num)
         print num,
         print " ", count/float(total)*100
-    NetworkWriter.writeToFile(net, file)
+
+if __name__ == "__main__":
+    readFile = True
+    tanh = False
+    regTrainedNetworks = ["1notanh.xml", "2notanh.xml", "3notanh.xml", "3notanhx1.xml", "3notanhx10.xml"]
+    tanhTrainedNetworks = ["1tanh.xml", "2tanh.xml", "3tanh.xml"]
+    if readFile:
+        for f in regTrainedNetworks + tanhTrainedNetworks:
+            if os.path.isfile(f):
+                print "reading network",
+                print f
+                net = NetworkReader.readFrom(f)
+                testNetwork(net)
+            else:
+                print "file",file,"does not exist"
+    else:
+        file = "newNetwork.xml"
+        train_ds = SupervisedDataSet(28 * 28, 1)
+        train_images, train_labels = mnist.load_mnist(dataset='training')
+        for image, label in zip(train_images, train_labels):
+            train_ds.addSample(ravel(image), label)
+
+        if tanh:
+            net = buildNetwork(train_ds.indim, 98, 98, 49, train_ds.outdim, bias=True, hiddenclass=TanhLayer)
+        else:
+            net = buildNetwork(train_ds.indim, 98, train_ds.outdim, bias=True)
+
+        trainer = BackpropTrainer(net, train_ds)
+
+        print "start training"
+        for i in range(5):
+            trainer.trainEpochs(1)
+            print "epoch: %4d" % trainer.totalepochs
+        testNetwork(net)
+        NetworkWriter.writeToFile(net, file)
